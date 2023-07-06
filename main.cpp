6fb54b01ee2ee5b24c715fb762d9c0c6c02afd94
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2023-07-07 02:10:59 +0300

    updated

--- main.cpp
+++ main.cpp
@@ -2,13 +2,16 @@
 
 
 /*
-    Negatif bir değere sahip bir port numarası kullanmak, ağ iletişimi standartlarına uygun olmadığı için hatalara neden olur ve beklenmedik sonuçlara yol açar. Bu nedenle, port numaraları her zaman pozitif tam sayı değerleri olarak kabul edilir.
-
+    Negatif bir değere sahip bir port numarası kullanmak, 
+    ağ iletişimi standartlarına uygun olmadığı için hatalara 
+    neden olur ve beklenmedik sonuçlara yol açar. 
+    Bu nedenle, port numaraları her zaman pozitif tam sayı 
+    değerleri olarak kabul edilir.
 */
 
 bool control(char **av)
-{ && 
-    if (atoi(av[1]) >= 0 && atoi(av[1]) <= 65535)//The maximum end port (based on the range being configured) cannot exceed 65535
+{
+    if (atoi(av[1]) >= 1024 && atoi(av[1]) <= 65535) //The maximum end port (based on the range being configured) cannot exceed 65535
         return true;
     return false;
 }
