69007a4a6b9f907f9a7896ddb44f6a1a4154c0d7
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2023-07-11 01:00:31 +0300

    updated

--- main.cpp
+++ main.cpp
@@ -1,52 +1,57 @@
 #include "./inc/Global.hpp"
 
-
 /*
-    Negatif bir değere sahip bir port numarası kullanmak, 
-    ağ iletişimi standartlarına uygun olmadığı için hatalara 
-    neden olur ve beklenmedik sonuçlara yol açar. 
-    Bu nedenle, port numaraları her zaman pozitif tam sayı 
+    Negatif bir değere sahip bir port numarası kullanmak,
+    ağ iletişimi standartlarına uygun olmadığı için hatalara
+    neden olur ve beklenmedik sonuçlara yol açar.
+    Bu nedenle, port numaraları her zaman pozitif tam sayı
     değerleri olarak kabul edilir.
 */
 
 bool control(char **av)
 {
-    if (atoi(av[1]) >= 1024 && atoi(av[1]) <= 65535) //The maximum end port (based on the range being configured) cannot exceed 65535
+    int port = atoi(av[2]);
+
+    if (port >= 1024 && port <= 65535)
         return true;
     return false;
 }
 
 int main(int ac, char **av)
 {
-	if (ac != 3 || !control(av))
-	{
-        cout << "You have entered the missing argument" << endl;
-        return (1);
-	}
-
-    Socket sc(atoi(av[1]));
-
-    sc.Create(atoi(av[1]));
-
-
-    if (!sc.Bind())
+    if (ac != 3 || !control(av))
     {
-        cout << "bind" << endl;
-        return (0);
-    }
-    if (!sc.Listen())
-    {
-        cout << "list" << endl;
+        cout << "You have entered the missing argument!" << endl;
         return (1);
     }
-    if (!sc.Accept(sc))
-    {
-        cout << "accept" << endl;
-        return (1);
-    }
-    
-    
-    cout << "it s over" << endl;
 
-	return (0);
+    string serverIP = av[1];
+    int serverPort = atoi(av[2]);
+    Socket clientSocket(serverPort);
+
+    if (!clientSocket.Create(serverPort))
+    {
+        cout << "Soket oluşturma hatası!" << endl;
+        return 1;
+    }
+
+    if (!clientSocket.Connect(serverIP))
+    {
+        cout << "Sunucuya bağlanma hatası!" << endl;
+        return 1;
+    }
+    else
+    {
+        cout << "Sunucuya bağlandı!" << endl;
+    }
+
+    string go;
+
+    while (go[0] != '.')
+    {
+        getline(cin, go);
+        clientSocket.Send(go);
+    }
+
+    return (0);
 }
