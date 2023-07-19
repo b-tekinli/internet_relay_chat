533bb5bce178262408d1a570df96085c837159dc
Kaano1 <ahmetyumusakdiken13@gmail.com>
2023-07-19 22:44:51 +0300

    added user data

--- inc/User.hpp
+++ inc/User.hpp
@@ -6,8 +6,23 @@
 class User
 {
 	private:
+		string user_name;
+		string host_name;
+		string serv_name;
+		string real_name;
+		string nick_name;
 		int grade;
 	public:
+		User();
+		User(string user_name, string host_name, string serv_name, string real_name, string nick_name);
+		User(const User &user);
+		~User();
+		User& operator=(const User &user);
+		const string& getUserName() const;
+		const string& getHostName() const;
+		const string& getServName() const;
+		const string& getRealName() const;
+		const string& getNickName() const;
 };
 
 #endif
