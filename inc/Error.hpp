aa56ef740ac71d0d2371c4e7b157a7951e3ef02c
rasnesakam <ensar.makas@gmail.com>
2023-07-30 15:35:39 +0300

    Error messages updated

--- inc/Error.hpp
+++ inc/Error.hpp
@@ -1,9 +1,9 @@
 #ifndef ERROR_HPP
 #define ERROR_HPP
 
-#define WR_PASS	"Server ERROR:: Incorrect Password\n"
-#define NO_PASS	"Server ERROR:: Your Connections Is Restricted"
-#define CH_NAME	"Server ERROR: Channel name fist characther must be '#'"
+#define WR_PASS	"Incorrect Password\n"
+#define NO_PASS	"Your Connections Is Restricted"
+#define CH_NAME	"Channel name fist characther must be '#'"
 
 
 #endif
