2ae8d4428ea9e68d3fcbf7113c1f9a55744e4b70
rasnesakam <ensar.makas@gmail.com>
2023-08-05 16:09:59 +0300

    Now, we read a line from fd instead of predefined size

--- inc/Server.hpp
+++ inc/Server.hpp
@@ -12,7 +12,6 @@ class Server
 		string							password;
 		map < string, vector<User*> >	channels;
 		vector <User*>					users;
-		const string* get_line(int fd);
 	public:
 		Server();
 		~Server();
