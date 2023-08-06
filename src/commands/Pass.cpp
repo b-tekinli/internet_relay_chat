27c91d8c5f28aea40423f3d31f964295ab2e2dfe
rasnesakam <ensar.makas@gmail.com>
2023-08-06 07:11:10 +0300

    Added codes to the messages

--- src/commands/Pass.cpp
+++ src/commands/Pass.cpp
@@ -5,22 +5,22 @@ int cmd::pass(const vector<string> &input, User& from)
 {
 	if (input.size() < 2)
 	{
-		Response::create().to(from).code(ERR_NEEDMOREPARAMS).content(input[0] + NOT_ENOUGH).send();
+		Response::withCode(ERR_NEEDMOREPARAMS).to(from).content(input[0] + NOT_ENOUGH).send();
 		return (-1);
 	}
 	else if (from.getActive() != FALSE)
 	{
-		Response::create().to(from).code(ERR_ALREADYREGISTRED).content(A_REGIST).send();
+		Response::withCode(ERR_ALREADYREGISTRED).to(from).content(A_REGIST).send();
 		return (-1);
 	}
 	if (isEqual(input[1], start.getPassword(), 1))
 	{
 		from.setActive(HALF);
-		Response::create().to(from).content("Password is correct\n").send();
+		Response::withCode(RPL_INFO).to(from).content("Password is correct\n").send();
 		cout << "correct" << endl;
 		return (1);
 	}
 	else
-		Response::create().to(from).content("Incorrect Password!\n").send();
+		Response::withCode(ERR_PASSWDMISMATCH).to(from).content("Incorrect Password!\n").send();
 	return (0);
 }
