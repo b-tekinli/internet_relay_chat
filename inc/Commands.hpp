117b2e0627c1a23f8cfe62b91a299cbd6ce7810e
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2023-08-07 21:40:35 +0300

    updated

--- inc/Commands.hpp
+++ inc/Commands.hpp
@@ -11,19 +11,17 @@ typedef int (*fp_command)(const vector<string> &input, User& user);
 namespace cmd
 {
 	int join(const vector<string> &input, User& user);
-	int nick(const vector<string> &input, User& user);
-	int quit(const vector<string> &input, User& user);
 	int kick(const vector<string> &input, User& user);
-	int ping(const vector<string> &input, User& user);
-	int pong(const vector<string> &input, User& user);
-	int	who(const  vector<string> &input, User& user);
-	int user(const vector<string> &input, User &user);
-	int mode(const vector<string> &input, User &user);
+	int nick(const vector<string> &input, User& user);
+	int list(const vector<string> &input, User &user);
 	int	pass(const vector<string> &input, User& from);
 	int	part(const vector<string> &input, User& user);
-	int list(const vector<string> &input, User &user);
 	int	notice(const vector<string> &input, User& from);
+	int ping(const vector<string> &input, User& user);
+	int pong(const vector<string> &input, User& user);
 	int privmsg(const vector<string> &input, User& user);
+	int quit(const vector<string> &input, User& user);
+	int user(const vector<string> &input, User &user);
 }
 
 bool	isEqual(const string &test, const string &aim, int enter);
