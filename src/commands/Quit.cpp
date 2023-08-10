86397c400d81703f717f5a21cbc207a11d197fad
Hatice Nur Eris <110325247+hnur-eris@users.noreply.github.com>
2023-08-10 20:19:01 +0300

    Update Quit.cpp
    quit yazıldı, onaylandı ve teste hazır hocalarım

--- src/commands/Quit.cpp
+++ src/commands/Quit.cpp
@@ -1,5 +1,26 @@
 #include "../../inc/Commands.hpp"
 
+//normal irc de kullanıcı çıkarken nedenini belirtiyorsa bizim bunu da yazdırmamız gerekiyomuş
+int cmd::quit(const vector <string> &input, User &user)
+{
+	if (input.size() > 1)
+	{
+		string reason = input[1];
+		cout << "User " << user.getNickName() << " is quitting with reason " << reason << endl;
+	}
+	else
+		cout << "User " << user.getNickName() << " is quitting " << endl;
+	
+	Response::create().to(user).code(RPL_WELCOME).content("GOOD BYE!!").send();
+	close(user.getFd());
+	
+	delete &user;
+	
+	return 0;
+}
+
+/*
+//beyza hocamızın denedigi
 int cmd::quit(const vector<string> &input, User& user)
 {
 	// string text;
@@ -19,6 +40,8 @@ int cmd::quit(const vector<string> &input, User& user)
 	cout << "quit" << endl;
 	return (0);
 }
+
+*/
 
 // void client_disconnect(int client_fd) {
 
