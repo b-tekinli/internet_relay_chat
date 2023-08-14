c5c29137e51009490288cce0a895e09aef1226ea
Kaano1 <ahmetyumusakdiken13@gmail.com>
2023-08-14 04:31:42 +0300

    Fixed: Kick command taked a wrong argument then it give a segfault

--- src/commands/Kick.cpp
+++ src/commands/Kick.cpp
@@ -1,5 +1,17 @@
 #include "../../inc/Commands.hpp"
 
+bool	inChannel(vector<User*> users, string name)
+{
+	int	i = 0;
+
+	for (; i < users.size(); i++)
+	{
+		if (users[i]->getNickName() == name)
+			return (true);
+	}
+	return (false);
+}
+
 int cmd::kick(const vector<string> &input, User& from)
 {
 	if (input.size() != 3)
@@ -7,14 +19,19 @@ int cmd::kick(const vector<string> &input, User& from)
 		Response::withCode(ERR_NEEDMOREPARAMS).to(from).content(KICK_USE).send();
 		return (-1);
 	}
-	else if (!find_channel(input[1], from))
+	if (!find_channel(input[1], from))
 	{
-		Response::withCode(ERR_NOSUCHCHANNEL).to(from).content(input[0] + NO_CHANNEL).send();
+		Response::withCode(ERR_NOSUCHCHANNEL).to(from).content(input[1] + NO_CHANNEL).send();
 		return (-1);
 	}
-	else if (!isEqual(start.getChannel(input[1])[0]->getNickName(), from.getNickName(), 1))
+	if (!isEqual(start.getChannel(input[1])[0]->getNickName(), from.getNickName(), 1))
 	{
 		Response::withCode(ERR_NOTONCHANNEL).to(from).content(input[0] + NO_OPER).send();
+		return (-1);
+	}
+	if (!inChannel(start.getChannel(input[1]), input[2]))
+	{
+		Response::withCode(ERR_USERNOTINCHANNEL).to(from).content(input[2] + " " + input[1] + USER_NOT_IN).send();
 		return (-1);
 	}
 	User							*to = start.getUserNick(input[2]);
